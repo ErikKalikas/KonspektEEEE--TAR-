8574200514425c7f8c0bc6e1bf52c820b5312c68
ErikKalikas <aazzeeqqssdd94@gmail.com>
2026-02-05 14:03:08 +0200

    upd 3

---
+++ KonspektEEE (TAR)/Class2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonspektEEE__TAR_
+{
+    internal class Osa2_funktsioonid
+    {
+
+        public static void Juku(string nimi1 = "Juku")
+        {
+            Console.WriteLine("mis on sinu nimi?");
+            string nimi = Console.ReadLine();
+
+            if (nimi == "Juku")
+            {
+                nimi1 += nimi;
+                Console.WriteLine("kirjuta oma sinu nimi");
+                int vanus = Convert.ToInt32(Console.ReadLine());
+                while (true)
+                {
+                    if (vanus < 6)
+                    {
+                        continue;
+                    }
+                    else if (vanus == 6)
+                    {
+                        Console.WriteLine("pilet on tasuta");                        
+                        break;
+                    }
+                    else if (vanus >= 7 && vanus <= 14)
+                    {
+                        Console.WriteLine("lastepilet");
+                        break;
+                    }
+                    else if (vanus >= 15 && vanus <=65)
+                    {
+                        Console.WriteLine("täispilet");
+                    }
+                }
+            }
+            else { }
+            
+        }
+    }
+}
--- KonspektEEE (TAR)/Program.cs
+++ KonspektEEE (TAR)/Program.cs
@@ -109,5 +109,6 @@ namespace Naidis_IKTpv25
         }
 
         
+        
     }
 }
