188a35ee077385fb42549d5643b44cf710cc17c3
ErikKalikas <aazzeeqqssdd94@gmail.com>
2026-02-05 12:55:52 +0200

    upd 1

---
+++ KonspektEEE (TAR)/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonspektEEE__TAR_
+{
+    internal class Naidis_fnktsion
+    {
+        public static float Summa (float arv1, float arv2)
+        {
+            return arv1 + arv2;
+        }
+    }
+}
---
+++ KonspektEEE (TAR)/Program.cs
@@ -0,0 +1,55 @@
+﻿using KonspektEEE__TAR_;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace Naidis_IKTpv25
+{
+    public class startPage
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("1. Osa Andmetüübid");
+            string tekst = "wew";
+            Console.WriteLine($"-> {tekst}");
+            Console.WriteLine($"-> {0}", tekst);
+
+            Console.Write("mis on sine nimi ");
+            string nimi = Console.ReadLine();           
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("kui vana sa oled?: ");
+                    int vanus = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"tere {nimi}. Sa olded {vanus} aastat vana");
+                    break;
+                }
+                catch (Exception e)
+                {
+                    continue;
+
+                }
+            }
+            try
+            {
+                Console.WriteLine("Arv 1:");
+                float a = float.Parse(Console.ReadLine());
+                Console.WriteLine("Arv 2 :");
+                float b = float.Parse(Console.ReadLine());
+                float vastus = Naidis_fnktsion.Summa(a, b);
+                Console.WriteLine($"Summa {a} ja {b} võrdub {vastus}");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e);
+            }
+
+
+        }
+        
+    }
+}
