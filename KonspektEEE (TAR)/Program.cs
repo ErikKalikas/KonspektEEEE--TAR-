fa934af2cd2c639f6e5cce1379d1181ad7381c39
Opilane TTHK <opilane@tthk.ee>
2026-02-25 12:45:27 +0200

    upd 5

--- KonspektEEE (TAR)/Program.cs
+++ KonspektEEE (TAR)/Program.cs
@@ -11,6 +11,97 @@ namespace Naidis_IKTpv25
     {              
         public static void Main(string[] args)
         {
+
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.WriteLine("valige osa");
+                Console.WriteLine("1");
+                Console.WriteLine("2");
+                Console.WriteLine("3");
+                int osa = int.Parse(Console.ReadLine());
+                Console.Clear();
+                if (osa == 1)
+                {
+                    
+
+                }
+                else if (osa == 2)
+                {
+
+                }
+                else if (osa == 3)
+                {
+                    Console.WriteLine("valige sektsioon");
+                    Console.WriteLine("1-13, (14)konspek ja (15)Teoria");
+                    int valik = int.Parse(Console.ReadLine());
+                    Console.Clear();
+                    switch (valik)
+                    {
+                        case 1:
+                            ArvuTöötlus.GenereeriRuudud();
+                            break;
+                        case 2:
+                            ArvuTöötlus.AnalüüsiArve();
+                            break;
+                        case 3:
+                                                        
+                            break;
+                        case 4:
+                            KuniMärksõnani();
+                            break;
+                        case 5:
+                            ArvamiseMäng();
+                            break;
+                        case 6:
+                            int[] arvud = ClassOsa3.SuurimNeliarvulineArv();
+                            ClassOsa3.SuurimNeliarv(arvud);
+                            break;
+                        case 7:
+                            var suurused = ClassOsa3.TabeliSisestaminee();
+                            ClassOsa3.GenereeriKorrutustabel(suurused.Item1, suurused.Item2);
+                            break;
+                        case 8:
+
+                            break;
+                        case 9:
+
+                            break;
+                        case 10:
+
+                            break;
+                        case 11:
+
+                            break;
+                        case 12:
+
+                            break;
+                        case 13:
+
+                            break;
+                        case 14:
+
+                            break;
+                        case 15:
+
+                            break;
+                        default:
+                            Console.WriteLine("vale");
+                            Environment.Exit(0);
+                            break;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("korda valik");
+                    continue;
+                }
+            }
+
+
+
             PaariJaPaaritudLoendused();
 
             //KõigeSuuremaArvuOtsing();
